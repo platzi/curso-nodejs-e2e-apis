a7944e520a461c1d20d21498f52fd3b1857eedb3
Nicolas Molina Monroy <hi@nicobytes.com>
2022-08-17 19:57:31 -0400

    :construction: recovery tests

--- e2e/auth.e2e.js
+++ e2e/auth.e2e.js
@@ -3,6 +3,18 @@ const createApp = require('./../src/app');
 const { models } = require('./../src/db/sequelize');
 const { upSeed, downSeed } = require('./utils/umzug');
 
+const mockSendMail = jest.fn();
+
+jest.mock('nodemailer', () => {
+  return {
+    createTransport: jest.fn().mockImplementation(() => {
+      return {
+        sendMail: mockSendMail,
+      }
+    })
+  }
+})
+
 describe('tests for /auth path', () => {
 
   let app = null;
@@ -41,6 +53,33 @@ describe('tests for /auth path', () => {
     });
 
   });
+
+  describe('POST /recovery', () => {
+
+    beforeAll(() => {
+      mockSendMail.mockClear();
+    })
+
+    test('should return a 401', async () => {
+      const inputData = {
+        email: "emailfake@gmail.com",
+      };
+      const { statusCode } = await api.post('/api/v1/auth/recovery').send(inputData);
+      expect(statusCode).toBe(401);
+    });
+
+    test('should send mail', async () => {
+      const user = await models.User.findByPk('1');
+      const inputData = {
+        email: user.email,
+      };
+      mockSendMail.mockResolvedValue(true);
+      const { statusCode, body } = await api.post('/api/v1/auth/recovery').send(inputData);
+      expect(statusCode).toBe(200);
+      expect(body.message).toEqual('mail sent');
+      expect(mockSendMail).toHaveBeenCalled();
+    });
+  })
 
 
   afterAll(async () => {
