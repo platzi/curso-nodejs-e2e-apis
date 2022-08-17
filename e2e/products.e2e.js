3d7d49c7cb1bfa8e78199fd2ccd4359d420c106b
Nicolas Molina Monroy <hi@nicobytes.com>
2022-08-17 19:35:13 -0400

    :construction: pagination

--- e2e/products.e2e.js
+++ e2e/products.e2e.js
@@ -24,6 +24,22 @@ describe('tests for /products path', () => {
       expect(body.length).toEqual(products.length);
       expect(body[0].category).toBeTruthy();
     });
+
+    test('should return 2 products with limit = 2 and offset = 0', async () => {
+      const limit = 2;
+      const offset = 0;
+      const { statusCode, body } = await api.get(`/api/v1/products?limit=${limit}&offset=${offset}`);
+      expect(statusCode).toEqual(200);
+      expect(body.length).toEqual(2);
+    });
+
+    test('should return 2 products with limit = 2 and offset = 2', async () => {
+      const limit = 2;
+      const offset = 2;
+      const { statusCode, body } = await api.get(`/api/v1/products?limit=${limit}&offset=${offset}`);
+      expect(statusCode).toEqual(200);
+      expect(body.length).toEqual(2);
+    });
   });
 
 
